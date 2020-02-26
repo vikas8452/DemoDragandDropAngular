a43372fe24fae87737fdd75683f6557be1a9b6dd
vikas.patel <vikas.patel@extramarks.com>
2020-02-26 19:06:09 +0530

    Added

---
+++ src/app/tree-flat-overview-example.ts
@@ -0,0 +1,626 @@
+import {FlatTreeControl} from '@angular/cdk/tree';
+import {Component, Injectable} from '@angular/core';
+import {MatTreeFlatDataSource, MatTreeFlattener} from '@angular/material/tree';
+import {BehaviorSubject, Observable, of as observableOf} from 'rxjs';
+import {CdkDragDrop} from '@angular/cdk/drag-drop';
+
+/**
+ * File node data with nested structure.
+ * Each node has a filename, and a type or a list of children.
+ */
+export class FileNode {
+  id: string;
+  children: FileNode[];
+  filename: string;
+  type: any;
+}
+
+/** Flat node with expandable and level information */
+export class FileFlatNode {
+  constructor(
+    public expandable: boolean,
+    public filename: string,
+    public level: number,
+    public type: any,
+    public id: string
+  ) {}
+}
+
+/**
+ * The file structure tree data in string. The data could be parsed into a Json object
+ */
+const TREE_DATA = JSON.stringify({
+  Applications: {
+    Calendar: 'app',
+    Chrome: 'app',
+    Webstorm: 'app'
+  },
+  Documents: {
+    angular: {
+      src: {
+        compiler: 'ts',
+        core: 'ts'
+      }
+    },
+    material2: {
+      src: {
+        button: 'ts',
+        checkbox: 'ts',
+        input: 'ts'
+      }
+    }
+  },
+  Downloads: {
+    October: 'pdf',
+    November: 'pdf',
+    Tutorial: 'html'
+  },
+  Pictures: {
+    'Photo Booth Library': {
+      Contents: 'dir',
+      Pictures: 'dir'
+    },
+    Sun: 'png',
+    Woods: 'jpg'
+  }
+});
+
+/**
+ * File database, it can build a tree structured Json object from string.
+ * Each node in Json object represents a file or a directory. For a file, it has filename and type.
+ * For a directory, it has filename and children (a list of files or directories).
+ * The input will be a json object string, and the output is a list of `FileNode` with nested
+ * structure.
+ */
+@Injectable()
+export class TreeDatabase {
+  dataChange = new BehaviorSubject<FileNode[]>([]);
+
+  get data(): FileNode[] { return this.dataChange.value; }
+
+  constructor() {
+    this.initialize();
+  }
+
+  initialize() {
+    // Parse the string to json object.
+    const dataObject = JSON.parse(TREE_DATA);
+
+    // Build the tree nodes from Json object. The result is a list of `FileNode` with nested
+    //     file node as children.
+    const data = this.buildFileTree(dataObject, 0);
+
+    // Notify the change.
+    this.dataChange.next(data);
+  }
+
+  /**
+   * Build the file structure tree. The `value` is the Json object, or a sub-tree of a Json object.
+   * The return value is the list of `FileNode`.
+   */
+  buildFileTree(obj: {[key: string]: any}, level: number, parentId: string = '0'): FileNode[] {
+    return Object.keys(obj).reduce<FileNode[]>((accumulator, key, idx) => {
+      const value = obj[key];
+      const node = new FileNode();
+      node.filename = key;
+      /**
+       * Make sure your node has an id so we can properly rearrange the tree during drag'n'drop.
+       * By passing parentId to buildFileTree, it constructs a path of indexes which make
+       * it possible find the exact sub-array that the node was grabbed from when dropped.
+       */
+      node.id = `${parentId}/${idx}`;
+
+      if (value != null) {
+        if (typeof value === 'object') {
+          node.children = this.buildFileTree(value, level + 1, node.id);
+        } else {
+          node.type = value;
+        }
+      }
+
+      return accumulator.concat(node);
+    }, []);
+  }
+}
+
+
+
+@Injectable()
+export class TreeDatabase1 {
+  dataChange = new BehaviorSubject<FileNode[]>([]);
+
+  get data(): FileNode[] { return this.dataChange.value; }
+
+  constructor() {
+    this.initialize();
+  }
+
+  initialize() {
+    // Parse the string to json object.
+    const dataObject = JSON.parse(TREE_DATA);
+
+    // Build the tree nodes from Json object. The result is a list of `FileNode` with nested
+    //     file node as children.
+    const data = this.buildFileTree(dataObject, 0);
+
+    // Notify the change.
+    this.dataChange.next(data);
+  }
+
+  /**
+   * Build the file structure tree. The `value` is the Json object, or a sub-tree of a Json object.
+   * The return value is the list of `FileNode`.
+   */
+  buildFileTree(obj: {[key: string]: any}, level: number, parentId: string = '0'): FileNode[] {
+    return Object.keys(obj).reduce<FileNode[]>((accumulator, key, idx) => {
+      const value = obj[key];
+      const node = new FileNode();
+      node.filename = key;
+      /**
+       * Make sure your node has an id so we can properly rearrange the tree during drag'n'drop.
+       * By passing parentId to buildFileTree, it constructs a path of indexes which make
+       * it possible find the exact sub-array that the node was grabbed from when dropped.
+       */
+      node.id = `${parentId}/${idx}`;
+
+      if (value != null) {
+        if (typeof value === 'object') {
+          node.children = this.buildFileTree(value, level + 1, node.id);
+        } else {
+          node.type = value;
+        }
+      }
+
+      return accumulator.concat(node);
+    }, []);
+  }
+}
+
+/**
+ * @title Tree with flat nodes
+ */
+@Component({
+  selector: 'tree-flat-overview-example',
+  templateUrl: 'tree-flat-overview-example.html',
+  styleUrls: ['tree-flat-overview-example.css'],
+  providers: [TreeDatabase,TreeDatabase1]
+})
+export class TreeFlatOverviewExample {
+
+
+  dragNode: any;
+  dragNodeExpandOverWaitTimeMs = 300;
+  dragNodeExpandOverNode: any;
+  dragNodeExpandOverTime: number;
+  dragNodeExpandOverArea: string;
+
+
+  // for tree 1
+  treeControl: FlatTreeControl<FileFlatNode>;
+  treeFlattener: MatTreeFlattener<FileNode, FileFlatNode>;
+  dataSource: MatTreeFlatDataSource<FileNode, FileFlatNode>;
+  expandedNodeSet = new Set<string>();
+  dragging = false;
+  expandTimeout: any;
+  expandDelay = 1000;
+
+
+  //for tree 2;
+  treeControl1: FlatTreeControl<FileFlatNode>;
+  treeFlattener1: MatTreeFlattener<FileNode, FileFlatNode>;
+  dataSource1: MatTreeFlatDataSource<FileNode, FileFlatNode>;
+  expandedNodeSet1 = new Set<string>();
+  dragging1 = false;
+  expandTimeout1: any;
+  expandDelay1= 1000;
+
+
+  constructor(treeDatabase: TreeDatabase,treeDatabase1: TreeDatabase1) {
+//for tree 1
+    this.treeFlattener = new MatTreeFlattener(this.transformer, this._getLevel,
+      this._isExpandable, this._getChildren);
+    this.treeControl = new FlatTreeControl<FileFlatNode>(this._getLevel, this._isExpandable);
+    this.dataSource = new MatTreeFlatDataSource(this.treeControl, this.treeFlattener);
+
+    treeDatabase.dataChange.subscribe(data => this.rebuildTreeForData(data));
+    
+//for tree 2
+    this.treeFlattener1 = new MatTreeFlattener(this.transformer1, this._getLevel1,
+      this._isExpandable1, this._getChildren1);
+    this.treeControl1 = new FlatTreeControl<FileFlatNode>(this._getLevel1, this._isExpandable1);
+    this.dataSource1 = new MatTreeFlatDataSource(this.treeControl1, this.treeFlattener1);
+
+
+    
+    treeDatabase1.dataChange.subscribe(data => this.rebuildTreeForData1(data));
+ 
+  }
+
+// for tree 1
+  transformer = (node: FileNode, level: number) => {
+    return new FileFlatNode(!!node.children, node.filename, level, node.type, node.id);
+  }
+
+  private _getLevel = (node: FileFlatNode) => node.level;
+  private _isExpandable = (node: FileFlatNode) => node.expandable;
+  private _getChildren = (node: FileNode): Observable<FileNode[]> => observableOf(node.children);
+  hasChild = (_: number, _nodeData: FileFlatNode) => _nodeData.expandable;
+
+
+
+  //for tree 2
+  transformer1 = (node: FileNode, level: number) => {
+    return new FileFlatNode(!!node.children, node.filename, level, node.type, node.id);
+  }
+  
+  private _getLevel1 = (node: FileFlatNode) => node.level;
+  private _isExpandable1 = (node: FileFlatNode) => node.expandable;
+  private _getChildren1 = (node: FileNode): Observable<FileNode[]> => observableOf(node.children);
+  hasChild1 = (_: number, _nodeData: FileFlatNode) => _nodeData.expandable;
+
+
+
+  /**
+   * This constructs an array of nodes that matches the DOM,
+   * and calls rememberExpandedTreeNodes to persist expand state
+   */
+  visibleNodes(): FileNode[] {
+    this.rememberExpandedTreeNodes(this.treeControl, this.expandedNodeSet);
+    const result = [];
+
+    function addExpandedChildren(node: FileNode, expanded: Set<string>) {
+      result.push(node);
+      if (expanded.has(node.id)) {
+        node.children.map(child => addExpandedChildren(child, expanded));
+      }
+    }
+    this.dataSource.data.forEach(node => {
+      addExpandedChildren(node, this.expandedNodeSet);
+    });
+    return result;
+  }
+
+// for tree 2
+
+visibleNodes1(): FileNode[] {
+  this.rememberExpandedTreeNodes(this.treeControl1, this.expandedNodeSet1);
+  const result = [];
+
+  function addExpandedChildren(node: FileNode, expanded: Set<string>) {
+    result.push(node);
+    if (expanded.has(node.id)) {
+      node.children.map(child => addExpandedChildren(child, expanded));
+    }
+  }
+
+  this.dataSource1.data.forEach(node => {
+    addExpandedChildren(node, this.expandedNodeSet1);
+  });
+  return result;
+}
+
+
+
+  /**
+   * Handle the drop - here we rearrange the data based on the drop event,
+   * then rebuild the tree.
+   * */
+  drop(event: CdkDragDrop<string[]>) {
+    // console.log('origin/destination', event.previousIndex, event.currentIndex);
+  console.log("In Tree 1",event);
+    // ignore drops outside of the tree
+    //if (!event.isPointerOverContainer) return;
+
+    // construct a list of visible nodes, this will match the DOM.
+    // the cdkDragDrop event.currentIndex jives with visible nodes.
+    // it calls rememberExpandedTreeNodes to persist expand state
+    const visibleNodes = this.visibleNodes();
+
+    // deep clone the data source so we can mutate it
+    const changedData = JSON.parse(JSON.stringify(this.dataSource.data));
+
+    // recursive find function to find siblings of node
+    function findNodeSiblings(arr: Array<any>, id: string): Array<any> {
+      let result, subResult;
+      arr.forEach(item => {
+        if (item.id === id) {
+          result = arr;
+        } else if (item.children) {
+          subResult = findNodeSiblings(item.children, id);
+          if (subResult) result = subResult;
+        }
+      });
+      return result;
+    }
+
+    // remove the node from its old place
+    const node = event.item.data;
+    const siblings = findNodeSiblings(changedData, node.id);
+    const siblingIndex = siblings.findIndex(n => n.id === node.id);
+    const nodeToInsert: FileNode = siblings.splice(siblingIndex, 1)[0];
+
+    // determine where to insert the node
+    const nodeAtDest = visibleNodes[event.currentIndex];
+    if (nodeAtDest.id === nodeToInsert.id) return;
+
+    // determine drop index relative to destination array
+    let relativeIndex = event.currentIndex; // default if no parent
+    const nodeAtDestFlatNode = this.treeControl.dataNodes.find(n => nodeAtDest.id === n.id);
+    const parent = this.getParentNode(nodeAtDestFlatNode);
+    if (parent) {
+      const parentIndex = visibleNodes.findIndex(n => n.id === parent.id) + 1;
+      relativeIndex = event.currentIndex - parentIndex;
+    }
+    // insert node 
+    const newSiblings = findNodeSiblings(changedData, nodeAtDest.id);
+    if (!newSiblings) return;
+    newSiblings.splice(relativeIndex, 0, nodeToInsert);
+    
+    // rebuild tree with mutated data
+    this.rebuildTreeForData(changedData);
+  }
+
+
+
+
+
+    /**
+   * Handle the drop - here we rearrange the data based on the drop event,
+   * then rebuild the tree.
+   * */
+
+
+
+   // For Tree 2
+  drop1(event: CdkDragDrop<string[]>) {
+    // console.log('origin/destination', event.previousIndex, event.currentIndex);
+    console.log("In Tree 2",event);
+    // ignore drops outside of the tree
+    
+    //if (!event.isPointerOverContainer) return;
+
+    // construct a list of visible nodes, this will match the DOM.
+    // the cdkDragDrop event.currentIndex jives with visible nodes.
+    // it calls rememberExpandedTreeNodes to persist expand state
+    const visibleNodes = this.visibleNodes();
+
+    const visibleNodes1 = this.visibleNodes1();
+
+    // deep clone the data source so we can mutate it
+    //dESTINATION tREE
+    const changedData = JSON.parse(JSON.stringify(this.dataSource.data));
+
+    console.log("DestTree",changedData);
+  // Source Tree
+    const changedData1 = JSON.parse(JSON.stringify(this.dataSource1.data));
+    console.log("Source Tree",changedData1);
+
+    // recursive find function to find siblings of node
+    function findNodeSiblings(arr: Array<any>, id: string): Array<any> {
+      let result, subResult;
+      arr.forEach(item => {
+        if (item.id === id) {
+          result = arr;
+        } else if (item.children) {
+          subResult = findNodeSiblings(item.children, id);
+          if (subResult) result = subResult;
+        }
+      });
+      return result;
+    }
+
+    // remove the node from its old place
+    const node = event.item.data;
+    const siblings = findNodeSiblings(changedData1, node.id);
+    const siblingIndex = siblings.findIndex(n => n.id === node.id);
+    const nodeToInsert: FileNode = siblings.splice(siblingIndex, 1)[0];
+
+    // determine where to insert the node
+    const nodeAtDest = visibleNodes1[event.currentIndex];
+
+    console.log("Node At Dest",nodeAtDest);
+
+    if (nodeAtDest.id === nodeToInsert.id) return;
+
+    // determine drop index relative to destination array
+    let relativeIndex = event.currentIndex; // default if no parent
+    
+    const nodeAtDestFlatNode = this.treeControl.dataNodes.find(n => nodeAtDest.id === n.id);
+    
+    const parent = this.getParentNode(nodeAtDestFlatNode);
+
+    console.log("Parent Of DestNode",parent);
+
+    if (parent) {
+      const parentIndex = visibleNodes.findIndex(n => n.id === parent.id) + 1;
+     // to chnage
+      relativeIndex = event.currentIndex - parentIndex;
+    }
+    // insert node 
+    const newSiblings = findNodeSiblings(changedData, nodeAtDest.id);
+
+    console.log("New Siblings 1",newSiblings);
+
+    if (!newSiblings) return;
+    newSiblings.splice(relativeIndex, 0, nodeToInsert);
+    
+    console.log("New Siblings 2",newSiblings)
+    // rebuild tree with mutated data
+
+console.log("Source Tree",changedData1);
+console.log("Dest Tree",changedData);
+
+    this.rebuildTreeForData1(changedData1);
+    this.rebuildTreeForData(changedData);
+  }
+
+  /**
+   * Experimental - opening tree nodes as you drag over them
+   */
+  dragStart() {
+    this.dragging = true;
+  }
+  dragEnd() {
+    this.dragging = false;
+  }
+  dragHover(node: FileFlatNode) {
+    if (this.dragging) {
+      clearTimeout(this.expandTimeout);
+      this.expandTimeout = setTimeout(() => {
+        this.treeControl.expand(node);
+      }, this.expandDelay);
+    }
+  }
+
+
+  dragHover1(node: FileFlatNode) {
+    if (this.dragging1) {
+      clearTimeout(this.expandTimeout1);
+      this.expandTimeout1 = setTimeout(() => {
+        this.treeControl1.expand(node);
+      }, this.expandDelay1);
+    }
+  }
+
+  dragHoverEnd() {
+    if (this.dragging) {
+      clearTimeout(this.expandTimeout);
+    }
+  }
+
+  /**
+   * The following methods are for persisting the tree expand state
+   * after being rebuilt
+   */
+
+  rebuildTreeForData(data: any) {
+    this.rememberExpandedTreeNodes(this.treeControl, this.expandedNodeSet);
+    this.dataSource.data = data;
+    this.forgetMissingExpandedNodes(this.treeControl, this.expandedNodeSet);
+    this.expandNodesById(this.treeControl.dataNodes, Array.from(this.expandedNodeSet));
+  }
+
+  rebuildTreeForData1(data: any) {
+    this.rememberExpandedTreeNodes(this.treeControl1, this.expandedNodeSet1);
+    this.dataSource1.data = data;
+    this.forgetMissingExpandedNodes(this.treeControl1, this.expandedNodeSet1);
+    this.expandNodesById1(this.treeControl1.dataNodes, Array.from(this.expandedNodeSet1));
+  }
+
+  private rememberExpandedTreeNodes(
+    treeControl: FlatTreeControl<FileFlatNode>,
+    expandedNodeSet: Set<string>
+  ) {
+    if (treeControl.dataNodes) {
+      treeControl.dataNodes.forEach((node) => {
+        if (treeControl.isExpandable(node) && treeControl.isExpanded(node)) {
+          // capture latest expanded state
+          expandedNodeSet.add(node.id);
+        }
+      });
+    }
+  }
+
+  private forgetMissingExpandedNodes(
+    treeControl: FlatTreeControl<FileFlatNode>,
+    expandedNodeSet: Set<string>
+  ) {
+    if (treeControl.dataNodes) {
+      expandedNodeSet.forEach((nodeId) => {
+        // maintain expanded node state
+        if (!treeControl.dataNodes.find((n) => n.id === nodeId)) {
+          // if the tree doesn't have the previous node, remove it from the expanded list
+          expandedNodeSet.delete(nodeId);
+        }
+      });
+    }
+  }
+
+  
+
+  private expandNodesById(flatNodes: FileFlatNode[], ids: string[]) {
+    if (!flatNodes || flatNodes.length === 0) return;
+    const idSet = new Set(ids);
+    return flatNodes.forEach((node) => {
+      if (idSet.has(node.id)) {
+        this.treeControl.expand(node);
+        let parent = this.getParentNode(node);
+        while (parent) {
+          this.treeControl.expand(parent);
+          parent = this.getParentNode(parent);
+        }
+      }
+    });
+  }
+
+  
+  private expandNodesById1(flatNodes: FileFlatNode[], ids: string[]) {
+    if (!flatNodes || flatNodes.length === 0) return;
+    const idSet = new Set(ids);
+    return flatNodes.forEach((node) => {
+      if (idSet.has(node.id)) {
+        this.treeControl1.expand(node);
+        let parent = this.getParentNode1(node);
+        while (parent) {
+          this.treeControl1.expand(parent);
+          parent = this.getParentNode1(parent);
+        }
+      }
+    });
+  }
+
+
+  private getParentNode(node: FileFlatNode): FileFlatNode | null {
+    const currentLevel = node.level;
+    if (currentLevel < 1) {
+      return null;
+    }
+    const startIndex = this.treeControl.dataNodes.indexOf(node) - 1;
+    for (let i = startIndex; i >= 0; i--) {
+      const currentNode = this.treeControl.dataNodes[i];
+      if (currentNode.level < currentLevel) {
+        return currentNode;
+      }
+    }
+    return null;
+  }
+
+  
+  private getParentNode1(node: FileFlatNode): FileFlatNode | null {
+    const currentLevel = node.level;
+    if (currentLevel < 1) {
+      return null;
+    }
+    const startIndex = this.treeControl1.dataNodes.indexOf(node) - 1;
+    for (let i = startIndex; i >= 0; i--) {
+      const currentNode = this.treeControl1.dataNodes[i];
+      if (currentNode.level < currentLevel) {
+        return currentNode;
+      }
+    }
+    return null;
+  }
+
+
+
+  handleDrop(event, node) {
+    event.preventDefault();
+    // if (node !== this.dragNode) {
+    //   let newItem: FileFlatNode;
+    //   if (this.dragNodeExpandOverArea === 'above') {
+    //     newItem = this.database.copyPasteItemAbove(this.flatNodeMap.get(this.dragNode), this.flatNodeMap.get(node));
+    //   } else if (this.dragNodeExpandOverArea === 'below') {
+    //     newItem = this.database.copyPasteItemBelow(this.flatNodeMap.get(this.dragNode), this.flatNodeMap.get(node));
+    //   } else {
+    //     newItem = this.database.copyPasteItem(this.flatNodeMap.get(this.dragNode), this.flatNodeMap.get(node));
+    //   }
+    //   this.database.deleteItem(this.flatNodeMap.get(this.dragNode));
+    //   this.treeControl.expandDescendants(this.nestedNodeMap.get(newItem));
+    // }
+    // this.dragNode = null;
+    // this.dragNodeExpandOverNode = null;
+    // this.dragNodeExpandOverTime = 0;
+
+    console.log("Handle Drop called");
+  }
+}
